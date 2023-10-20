8b843a4959e139a0912118f31b3fadf2b17edeea
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-10-20 14:51:22 -0300

    feat: find address by id controller route

--- src/application/house/address/address.controller.ts
+++ src/application/house/address/address.controller.ts
@@ -4,11 +4,20 @@ import { RoleGuard } from '@application/@guards/role.guard';
 import { CreateAddressDTO } from '@common/dtos/house.dto';
 import { Role } from '@common/enums/role.enum';
 import { CreateAddressService } from '@domain/house/address/services/create-address.service';
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { FindAddressService } from '@domain/house/address/services/find-address.service';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 
 @Controller('address')
 export class AddressController {
-  constructor(private createAddressService: CreateAddressService) {}
+  constructor(
+    private createAddressService: CreateAddressService,
+    private findAddressService: FindAddressService,
+  ) {}
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.findAddressService.execute({ id });
+  }
 
   @Post()
   @Roles(Role.LANDLORD)
